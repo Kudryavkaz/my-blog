68edc059dc7686a5945f3ea9b0bc82003cbd8b22
zhangfish <noumi_kudryavka@foxmail.com>
2024-09-07 18:52:34 +0800

    🎉 init: init my blog

---
+++ www/usr/themes/default/footer.php
@@ -0,0 +1,16 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+
+        </div><!-- end .row -->
+    </div>
+</div><!-- end #body -->
+
+<footer id="footer" role="contentinfo">
+    &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
+    <?php _e('由 <a href="https://typecho.org">Typecho</a> 强力驱动'); ?>.
+    <div class="blog-text-center"><a href="https://beian.miit.gov.cn/" target="_blank" rel="nofollow"><?php $this->options->bei(); ?></a></div>
+</footer>
+<!-- end #footer -->
+
+<?php $this->footer(); ?>
+</body>
+</html>
